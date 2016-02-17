6e176fcd0ab39eb6ebabacb08da0457c210aa67f
Dean Chuang <azurenokite@gmail.com>
2016-02-17 23:28:50 +0000

    update posts general styling

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :update, :destroy]
   
   def index
-    @posts = Post.all
+    @posts = Post.created_descending
   end
   
   def new
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ActiveRecord::Base
   
   validates :title, :content, presence: :true
+  
+  def self.created_descending
+    all.order("created_at desc") 
+  end
 end
