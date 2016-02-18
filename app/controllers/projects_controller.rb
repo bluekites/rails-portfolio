7b7f3387d34db826b8e7289bfc3a75695c9f90f0
Dean Chuang <azurenokite@gmail.com>
2016-02-18 03:30:09 +0000

    setup project model and basic styling

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,44 @@
+class ProjectsController < ApplicationController
+  before_action :find_project, only: [:edit, :show, :update, :destroy]
+  
+  def index
+    @projects = Project.all.order("created_at desc")
+  end
+  
+  def new
+    @project = Project.new
+  end
+  
+  def create
+    @project = Project.create(project_params)
+    if @project.save
+      redirect_to project_path(@project), notice: "Success"
+    else
+      render :new
+    end
+  end 
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def update
+  
+  end
+
+  def destroy
+    @project.destroy
+    redirect_to projects_path
+  end
+  
+  private
+    def project_params
+      params.require(:project).permit(:title, :description, :link)
+    end
+    
+    def find_project
+      @project = Project.find(params[:id])
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160218011043) do
+ActiveRecord::Schema.define(version: 20160218020143) do
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string   "slug",                      null: false
@@ -35,5 +35,13 @@ ActiveRecord::Schema.define(version: 20160218011043) do
   end
 
   add_index "posts", ["slug"], name: "index_posts_on_slug", unique: true
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "link"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
 end
