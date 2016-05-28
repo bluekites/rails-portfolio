c57b307512799f748b5f9cce735dd6a91cfc5a7d
Dean Chuang <azurenokite@gmail.com>
2016-05-28 18:47:47 +0000

    increase limit to front page post to 5

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @posts = Post.all.order("created_at desc").limit(4)
-    @projects = Project.all.order("created_at desc").limit(4)
+    @posts = Post.all.order("created_at desc").limit(5)
+    @projects = Project.all.order("created_at desc").limit(5)
   end
 end
